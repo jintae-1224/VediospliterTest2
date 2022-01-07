8ecf59c9a37f6888da030c8a7e07aa42aafc74e3
DS <DS@DESKTOP-J4KN1KI>
2022-01-07 14:16:03 +0900

    초기 커밋.

---
+++ VediospliterTest/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace VediospliterTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_fileName = new System.Windows.Forms.TextBox();
+            this.btn_SaveFolder = new System.Windows.Forms.Button();
+            this.lb_savePath = new System.Windows.Forms.Label();
+            this.btn_OpenCSV = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_spliteTime = new System.Windows.Forms.TextBox();
+            this.btn_Splite = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lb_progressText = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(41, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "파일명 : ";
+            // 
+            // txt_fileName
+            // 
+            this.txt_fileName.Location = new System.Drawing.Point(121, 22);
+            this.txt_fileName.Name = "txt_fileName";
+            this.txt_fileName.Size = new System.Drawing.Size(458, 25);
+            this.txt_fileName.TabIndex = 1;
+            // 
+            // btn_SaveFolder
+            // 
+            this.btn_SaveFolder.Location = new System.Drawing.Point(611, 22);
+            this.btn_SaveFolder.Name = "btn_SaveFolder";
+            this.btn_SaveFolder.Size = new System.Drawing.Size(89, 26);
+            this.btn_SaveFolder.TabIndex = 2;
+            this.btn_SaveFolder.Text = "저장경로";
+            this.btn_SaveFolder.UseVisualStyleBackColor = true;
+            this.btn_SaveFolder.Click += new System.EventHandler(this.btn_SaveFolder_Click);
+            // 
+            // lb_savePath
+            // 
+            this.lb_savePath.AutoSize = true;
+            this.lb_savePath.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lb_savePath.Location = new System.Drawing.Point(44, 71);
+            this.lb_savePath.Name = "lb_savePath";
+            this.lb_savePath.Size = new System.Drawing.Size(0, 17);
+            this.lb_savePath.TabIndex = 3;
+            // 
+            // btn_OpenCSV
+            // 
+            this.btn_OpenCSV.Location = new System.Drawing.Point(44, 112);
+            this.btn_OpenCSV.Name = "btn_OpenCSV";
+            this.btn_OpenCSV.Size = new System.Drawing.Size(107, 28);
+            this.btn_OpenCSV.TabIndex = 4;
+            this.btn_OpenCSV.Text = "OpenCSV";
+            this.btn_OpenCSV.UseVisualStyleBackColor = true;
+            this.btn_OpenCSV.Click += new System.EventHandler(this.btn_OpenCSV_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(171, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "저장간격 : ";
+            // 
+            // txt_spliteTime
+            // 
+            this.txt_spliteTime.Location = new System.Drawing.Point(268, 113);
+            this.txt_spliteTime.Name = "txt_spliteTime";
+            this.txt_spliteTime.Size = new System.Drawing.Size(100, 25);
+            this.txt_spliteTime.TabIndex = 6;
+            this.txt_spliteTime.Text = "8";
+            // 
+            // btn_Splite
+            // 
+            this.btn_Splite.Location = new System.Drawing.Point(611, 112);
+            this.btn_Splite.Name = "btn_Splite";
+            this.btn_Splite.Size = new System.Drawing.Size(89, 27);
+            this.btn_Splite.TabIndex = 7;
+            this.btn_Splite.Text = "Splite";
+            this.btn_Splite.UseVisualStyleBackColor = true;
+            this.btn_Splite.Click += new System.EventHandler(this.btn_Splite_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(44, 187);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(656, 23);
+            this.progressBar.TabIndex = 8;
+            // 
+            // lb_progressText
+            // 
+            this.lb_progressText.AutoSize = true;
+            this.lb_progressText.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lb_progressText.Location = new System.Drawing.Point(44, 162);
+            this.lb_progressText.Name = "lb_progressText";
+            this.lb_progressText.Size = new System.Drawing.Size(0, 21);
+            this.lb_progressText.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(477, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(102, 25);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(400, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Start : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 229);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_progressText);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btn_Splite);
+            this.Controls.Add(this.txt_spliteTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_OpenCSV);
+            this.Controls.Add(this.lb_savePath);
+            this.Controls.Add(this.btn_SaveFolder);
+            this.Controls.Add(this.txt_fileName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_fileName;
+        private System.Windows.Forms.Button btn_SaveFolder;
+        private System.Windows.Forms.Label lb_savePath;
+        private System.Windows.Forms.Button btn_OpenCSV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_spliteTime;
+        private System.Windows.Forms.Button btn_Splite;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lb_progressText;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ VediospliterTest/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VediospliterTest
+{
+    public partial class Form1 : Form
+    {
+        List<String> list = new List<String>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar.Visible = false;
+        }
+
+        private void btn_SaveFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                lb_savePath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void btn_OpenCSV_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog fd = new OpenFileDialog())//파일 선택 창 열기
+            {
+                fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // 초기화면
+                fd.Filter = "CSV Files (*.csv)|*.csv";    // 필터
+                fd.FilterIndex = 1;
+
+                if (fd.ShowDialog() == DialogResult.OK)//확인 버튼 누르면
+                {
+                    filePath = fd.FileName;
+                    StreamReader sr = new StreamReader(filePath);//선택된 파일 읽기
+
+                    while (!sr.EndOfStream)//줄이 끝날 때까지
+                    {
+                        // 한 줄씩 읽어온다.
+                        string line = sr.ReadLine(); //한 줄 읽음) 1,11/01/2021,06:31:02 PM,1,1
+
+                        // 쉼표( , )를 기준으로 데이터를 분리한다.
+                        string[] data = line.Split(',');//한 줄을 배열로 분리해서 data 배열에 저장
+                                                        //1
+                                                        //11/01/2021
+                                                        //06:31:02 PM
+                                                        //1
+                                                        //1
+                       
+
+                        for (int i = 0; i < data.Length; i++)
+                        {
+                            if (i == 2)
+                            {
+                                list.Add((data[i]).ToString());
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+        private void btn_Splite_Click(object sender, EventArgs e)
+        {
+            var filePathVedio = string.Empty;
+
+            progressBar.Minimum = 0;
+            progressBar.Maximum = list.Count;
+            progressBar.Step = 1;
+            progressBar.Value = 0;
+
+            using (OpenFileDialog fdVedio = new OpenFileDialog())
+            {
+                fdVedio.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // 초기화면
+                fdVedio.Filter = "Video Files | *.mp4; *.wmv; *.avi";    // 필터
+                fdVedio.FilterIndex = 1;
+
+                if (fdVedio.ShowDialog() == DialogResult.OK)
+                {
+                    filePathVedio = fdVedio.FileName;
+                    progressBar.Visible = true;
+                    progressBar.PerformStep();
+
+                    //여기 for
+                    DateTime excel = Convert.ToDateTime(list[1]);//엑셀에서공 던지는 시간
+                    DateTime video = Convert.ToDateTime(textBox1.Text + "PM");//비디오에서 공 던지는 시간   
+
+                    TimeSpan dateDiff = excel - video;
+
+                    int realvideohour = dateDiff.Hours;//6
+                    int realvideomin = dateDiff.Minutes;//25
+                    int realvideosec = dateDiff.Seconds;//34
+                    
+                    //int total = (realvideohour*60*60) + (realvideomin*60) + realvideosec;
+
+                    //MessageBox.Show(total.ToString());
+
+                    DateTime finalvideo = Convert.ToDateTime(realvideohour + ":" + realvideomin + ":" + realvideosec + "PM");
+
+                    for (int i = 1; i < list.Count; i++)
+                    {
+                        DateTime excel2 = Convert.ToDateTime(list[i]);
+
+                        TimeSpan realvideo = excel2 - finalvideo;
+
+                        int totalhour = realvideo.Hours * 60 * 60;
+                        int totalmin = realvideo.Minutes * 60;
+                        int totalsec = realvideo.Seconds;
+
+                        int total = totalhour + totalmin + totalsec;
+
+                        int start = Convert.ToInt16(total);
+                        int end = Convert.ToInt16(txt_spliteTime.Text);
+
+                        string outputFilename = $@"{lb_savePath.Text}\{start}.mp4";
+                        string command = $"-i \"{filePathVedio}\" -ss {start} -t {end} \"{outputFilename}\"";
+
+                        Process process = new Process();
+
+                        process.StartInfo.FileName = @"cmd.exe";
+                        process.StartInfo.Arguments = $@"/k ffmpeg.dll {command} & exit";
+
+                        process.StartInfo.CreateNoWindow = false;
+                        process.StartInfo.UseShellExecute = false;
+
+                        process.Start();
+                        process.WaitForExit();
+
+                        progressBar.PerformStep();
+                        lb_progressText.Text = $"{i}/{list.Count-1}";    
+                    }
+                    lb_progressText.Text = "Done";
+                    Process.Start("explorer.exe", "/select, \"" + $@"{lb_savePath.Text}");
+                }
+            }
+        }
+    }
+}
